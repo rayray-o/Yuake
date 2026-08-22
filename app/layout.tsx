d0c4f9384ef82f1e2e2fe4c1513a3b14db5d9c64
Ray <d2arknotfound@gmail.com>
2026-08-22 22:12:01 +0530

    Add metadata and global styles to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "YUAKE",
+  description: "A camera-driven interactive reality."
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
